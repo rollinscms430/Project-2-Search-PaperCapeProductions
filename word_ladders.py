7a1bf03fa80e1814c82faa331c98772e424c674e
Nick LHeureux <nlheureux@rollins.edu>
2017-03-05 19:16:33 +0000

    Changed code to create dictionary then using a bfs find the shortest path.

--- word_ladders.py
+++ word_ladders.py
@@ -1,58 +1,86 @@
+"""Holds information for each state including the frontier and returns the next element in the frontier"""
 class State(object):
     def __init__(self, name):
         self.name = name
         self.list = []
-        self.is_parent = False
         self.count = 0
-        self.parent = self
-        self.complete = False
-        self.next = ''
-
-    def makeParent(self):
-        self.is_parent = True
     
-    def addParent(self, parent):
-        self.parent = parent
+    def get_name(self):
+        return self.name
         
-    def getParent(self):
-        return self.parent
-        
-    def isComplete(self):
-        return self.complete
-        
-    def addWord(self, word):
+    def add_to_frontier(self, word):
         self.list.append(word)
     
-    def setIncrement(self, count):
-        self.count = count
+    def get_next_element(self):
+        index = self.count
+        self.count += 1
+        length = len(self.get_frontier())
+        if length > index:
+            return self.list[index]
     
-    def getIncrement(self):
-        return self.count
-    
-    def getNext(self):
-        if len(self.getList()) > self.getIncrement():
-            self.next = self.getList()[self.count]
-            print self.getIncrement()
-            return self.next
-        else: 
-            self.complete = True
-            return None
-            
-    def getList(self):
+    def get_frontier(self):
         return self.list
-        
-    # toString
-    def __str__(self):
-        s = 'State ' + self.name + '\n'
-        return s
 
-def buildState(state, possible_words):
+"""Implements a Stack with the usual methods"""
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
+
+def build_state(state, possible_words):
     for word in possible_words:
-        if isValid(word, state.name):
-            state.addWord(word)
+        if is_valid(word, state.get_name()):
+            state.add_to_frontier(word)
     return state
 
-        
+    
+def is_valid(word, state_name):
+    count = 0
+    if word == state_name:
+        return False
+    for index in range(len(state_name)):
+        if word[index] != state_name[index]:
+            count += 1
+        if count > 1:
+            return False
+    return True
+    
+def create_dict(state, dict, goal):
+    new_dict = {}
+    state = build_state(state, dict)
+
+    stack = Stack()
+    visited = [] 
+    stack.push(state)
+    while not stack.isEmpty():
+        current = stack.pop()
+        new_dict[current.get_name()] = current.get_frontier()
+        if current.get_name() in visited:
+            continue
+        else:
+            visited.append(current.get_name())
+            for node in current.get_frontier():
+                state = State(node)
+                new_state = build_state(state, dict)
+                stack.push(new_state)
+
+    return new_dict
+    
 def bfs(states, start, end):
     queue = []
     queue.append([start])
@@ -66,55 +94,11 @@ def bfs(states, start, end):
             new_path.append(adjacent)
             queue.append(new_path)
 
-
-def isValid(word, state_name):
-    count = 0
-    if word == state_name:
-        return False
-    for index in range(len(state_name)):
-        if word[index] != state_name[index]:
-            count += 1
-        if count > 1:
-            return False
-    return True
-
-def createDictionary(possible_words, start, goal):
-    state = State(start)
-    dict = {}
-    state.makeParent()
-    new_state = buildState(state, possible_words)
-    dict[new_state.name] = new_state.getList()
-    next = new_state.getNext()
-    
-    print 'next', next
-
-    while not new_state.getParent().isComplete() or not next != goal:
-        if not new_state.getParent().isComplete():
-            state = State(next)
-            state.addParent(new_state);
-            parent = new_state
-            new_state = buildState(state, possible_words)
-            dict[new_state.name] = new_state.getList() 
-            next = parent.getNext()
-            
-            print 'next2', next
-            print dict
-        else:
-            next_child = new_state.getParent().getList()[0]
-            next_child.addParent(new_state)
-            next_child = buildState(state, possible_words)
-            dict[next_child.name] = next_child.getList()
-            new_state = next_child
-            break
-
-    print dict.keys()
-    return dict
-
-    
+          
 def main():
     dictionary = {}
-    start = 'egg'
-    goal = 'eng'
+    start = 'snakes'
+    goal = 'brains'
     file = open('words.txt')
     for word in file:
         dictionary[word.split()[0]] = word.split()
@@ -123,15 +107,10 @@ def main():
     for word in dictionary:
         if len(word) == len(goal):
             possible_words[word] = word
+    state = State(start)
+    states = create_dict(state, possible_words, goal) 
+    print 'Shortest path', bfs(states, start, goal)
     
-    dict = createDictionary(possible_words, start, goal)
-    
-    #print bfs(dict, start, goal)
-        
+
 main()
         
-
-    
-    
-    
-
